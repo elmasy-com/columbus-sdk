003f8abdb06078098262d6d7031976f97afab92c
g0rbe <mail@danielgorbe.com>
2022-11-05 23:30:58 +0100

    add domain package

---
+++ domain/domain.go
@@ -0,0 +1,26 @@
+package domain
+
+import "strings"
+
+type Domain struct {
+	Domain string   `bson:"domain" json:"domain"`
+	Shard  int      `bson:"shard" json:"shard"`
+	Subs   []string `bson:"subs" json:"subs"`
+}
+
+// GetFull resturns the hostnames as a slice of string.
+// If Subs is empty returns nil (theoretically impossible).
+func (d *Domain) GetFull() []string {
+
+	var list []string
+
+	for i := range d.Subs {
+		if d.Subs[i] == "" {
+			list = append(list, d.Domain)
+		} else {
+			list = append(list, strings.Join([]string{d.Subs[i], d.Domain}, "."))
+		}
+	}
+
+	return list
+}
