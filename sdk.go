58f841399bb26268c9be356d4b6debc3e17e8231
g0rbe <mail@danielgorbe.com>
2023-01-26 19:33:54 +0100

    remove user related packages

--- sdk.go
+++ sdk.go
@@ -65,39 +65,39 @@ func Lookup(d string, full bool) ([]string, error) {
 	return subs, err
 }
 
-// Insert inserts d into the database.
-// Uses the DefaultUser to do the query.
-//
-// Known errors are:
-// ErrInvalidDomain (d is not a vlid domain), ErrDefaultUserNil (DefaultUser is not set),
-// ErrBlocked (blocked IP), ErrMissingAPIKey (API key is missing) and ErrInvalidAPIKey (API key is invalid).
-func Insert(d string) error {
+// // Insert inserts d into the database.
+// // Uses the DefaultUser to do the query.
+// //
+// // Known errors are:
+// // ErrInvalidDomain (d is not a vlid domain), ErrDefaultUserNil (DefaultUser is not set),
+// // ErrBlocked (blocked IP), ErrMissingAPIKey (API key is missing) and ErrInvalidAPIKey (API key is invalid).
+// func Insert(d string) error {
 
-	if !domain.IsValid(d) {
-		return fault.ErrInvalidDomain
-	}
-	if DefaultUser == nil {
-		return fault.ErrDefaultUserNil
-	}
-	if DefaultUser.Key == "" {
-		return fault.ErrMissingAPIKey
-	}
+// 	if !domain.IsValid(d) {
+// 		return fault.ErrInvalidDomain
+// 	}
+// 	if DefaultUser == nil {
+// 		return fault.ErrDefaultUserNil
+// 	}
+// 	if DefaultUser.Key == "" {
+// 		return fault.ErrMissingAPIKey
+// 	}
 
-	path := uri + "/insert/" + d
+// 	path := uri + "/insert/" + d
 
-	req, err := http.NewRequest("PUT", path, nil)
-	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
-	}
+// 	req, err := http.NewRequest("PUT", path, nil)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to create request: %w", err)
+// 	}
 
-	req.Header.Set("User-Agent", UserAgent)
-	req.Header.Set("X-Api-Key", DefaultUser.Key)
+// 	req.Header.Set("User-Agent", UserAgent)
+// 	req.Header.Set("X-Api-Key", DefaultUser.Key)
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("request failed: %w", err)
-	}
-	defer resp.Body.Close()
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return fmt.Errorf("request failed: %w", err)
+// 	}
+// 	defer resp.Body.Close()
 
-	return handleResponse(resp, nil)
-}
+// 	return handleResponse(resp, nil)
+// }
--- user/user.go
+++
@@ -1,7 +0,0 @@
-package user
-
-type User struct {
-	Key   string `bson:"key" json:"key"`
-	Name  string `bson:"name" json:"name"`
-	Admin bool   `bson:"admin" json:"admin"`
-}
